8fd75f2290cf1ee7be700b1fcbe7bd464ada6240
Yudi Widiawan <Yudi Widiawan>
2017-04-22 11:17:54 +0700

    Added abstract class and interface

---
+++ Aritmatika/src/aritmatika/Operations.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aritmatika;
+
+/**
+ *
+ * @author makan
+ */
+public interface Operations {
+    public float operate(int x, int y);
+    public void display();
+}
